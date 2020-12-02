e7af8b76cf8714e7e0614e3594ddc49e75f5a2af
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 03:52:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  out = nil 
+  name_hash.each do |key, value|
+    if(out == nil)
+      out = value 
+    elsif(value<out)
+      out = value
+    end
+  return out
 end
